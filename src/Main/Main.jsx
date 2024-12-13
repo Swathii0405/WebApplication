e3392cd4bc158205632735756057f163d3e20fd9
Swathii0405 <swathichithiravel2004@gmail.com>
2024-12-13 19:11:41 +0530

    Update Main.jsx

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,61 +1,63 @@
 
 import "./Main.css";
-const Main =(props)=>{
+import "./Booking.css";
+import React, { useState } from "react";
 
-    return(
-        <>
-       <div className={props.cName}>
-       </div>
-        <div className="main-text">
-            <h1>{props.tittle}</h1>
-            <p>{props.text}</p>
-            <a href={props.url}className={props.btnClass}>
-                {props.buttonText}
-            </a>
+const Main = (props) => {
+  const [bookingSuccess, setBookingSuccess] = useState(false);
 
-             <div className="empty1"></div>
+  
+  const handleBooking = () => {
+    setBookingSuccess(true);  
+    setTimeout(() => {
+      setBookingSuccess(false); 
+    }, 3000);  
+  };
 
-                    <section id="location-search">
-                    <div claassName="container">
-                        <div className="form-wrapper">
-                            <form className="des-duration">
-                                <input type="Date"className="form-control"/>
-                                <select className="form-control">
-                                  <div className="empty2"></div>
-                                <option value="Destination">Destination</option>
-                                <option value="Destination">Paris</option>
-                                <option value="Destination">Maldives</option>
-                                <option value="Destination">Switzerland</option>
-                                </select>
+  return (
+    <>
+      <div className={props.cName}></div>
+      <div className="main-text">
+        <h1>{props.tittle}</h1>
+        <p>{props.text}</p>
+        <a href={props.url} className={props.btnClass}>
+          {props.buttonText}
+        </a>
 
+        <div className="empty1"></div>
+        <section>
+          <div className="container">
+            <div className="form-wrapper">
+              <form className="des-durations">
+                
+                <input type="date" className="form-control" />
+                <select className="form-control">
+                  <option value="Destination">Destination</option>
+                  <option value="Paris">Paris</option>
+                  <option value="Maldives">Maldives</option>
+                  <option value="Switzerland">Switzerland</option>
+                </select>
+                <select className="form-control">
+                  <option value="Duration">Duration</option>
+                  <option value="2-4 Days">2-4 Days Tour</option>
+                  <option value="5-7 Days">5-7 Days Tour</option>
+                  <option value="7+ Days">7+ Days</option>
+                </select>
 
-                                <select className="form-control">
-                                <div className="empty2"></div>
-                                <option value="Duration">Duration</option>
-                                <option value="2-4 Days">2-4 Days Tour</option>
-                                <option value="5-7 Days">5-7 Days Tour</option>
-                                <option value="7+ Days">7+ Days</option>
-                                </select>
+                
+                <button type="button" className="book-now-btn" onClick={handleBooking}>
+                  Book Now
+                </button>
+              </form>
 
-                            </form>
-
-                        </div>
-
-                    </div>
-                   
-
-                </section> 
-                {/* <button claassName="sear-btn">Search Now</button> */}
-
-               
- 
-
-
-
-
-</div>
-        </>
-    )
-}
+              
+              {bookingSuccess && <div id="booking-message">Booking Successfully!</div>}
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
 export default Main;
